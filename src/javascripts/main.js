87884c31a035399ba8bb86ba42bbe1ab440fcf01
Steven Zelenak <stevenmzelenak@gmail.com>
2020-03-21 11:06:49 -0500

    login button

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,4 +1,14 @@
 import 'bootstrap';
 import '../styles/main.scss';
+import firebase from 'firebase/app';
+import apiKeys from './helpers/apiKeys.json';
+import auth from './components/auth/auth';
+import authData from './helpers/data/authData';
 
-console.error('hi');
+const init = () => {
+  firebase.initializeApp(apiKeys.firebaseKeys);
+  authData.checkLoginStatus();
+  auth.loginButton();
+};
+
+init();
