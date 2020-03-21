d23508373a58db62707bba77e3653e5478882664
Steven Zelenak <stevenmzelenak@gmail.com>
2020-03-21 11:14:45 -0500

    logout button

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -4,11 +4,13 @@ import firebase from 'firebase/app';
 import apiKeys from './helpers/apiKeys.json';
 import auth from './components/auth/auth';
 import authData from './helpers/data/authData';
+import myNavBar from './components/myNavbar/myNavBar';
 
 const init = () => {
   firebase.initializeApp(apiKeys.firebaseKeys);
   authData.checkLoginStatus();
   auth.loginButton();
+  myNavBar.logoutEvent();
 };
 
 init();
