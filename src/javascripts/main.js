9af52243b564c60446a380fe89ece1f4e85d3875
Steven Zelenak <stevenmzelenak@gmail.com>
2020-03-23 17:22:41 -0500

    wip

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,7 +1,9 @@
 import 'bootstrap';
 import '../styles/main.scss';
+
 import firebase from 'firebase/app';
 import apiKeys from './helpers/apiKeys.json';
+
 import auth from './components/auth/auth';
 import authData from './helpers/data/authData';
 import myNavBar from './components/myNavbar/myNavBar';
