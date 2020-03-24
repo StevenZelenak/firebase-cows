59be81d10735f582190a4155c73d2b08b0122ddc
Steven Zelenak <stevenmzelenak@gmail.com>
2020-03-24 18:00:00 -0500

    update

--- src/javascripts/helpers/data/cowData.js
+++ src/javascripts/helpers/data/cowData.js
@@ -6,14 +6,9 @@ const baseUrl = apiKeys.firebaseKeys.databaseURL;
 
 const getCows = () => new Promise((resolve, reject) => {
   axios.get(`${baseUrl}/cows.json`)
-    //If we get the info from where we need it. The (.then) activates.
-    //we name and assign the parameter of (.then)'s value at the same time. 
     .then((response) => {
-      //we assign the info that comes back from the server into a variable
       const demCows = response.data;
-      //we create a empty array
       const cows = [];
-      //We grab the object keys of response(server object data)
       Object.keys(demCows).forEach((cowId) => {
         demCows[cowId].id = cowId;
         cows.push(demCows[cowId]);
